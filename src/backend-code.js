3b761578b1b34a7ab400d30e5d726ceb3ab5568b
Jessica Munoz and Na Hyung Choi <jessica+na@>
2019-06-26 11:12:37 -0700

    covert year to a form that can be used in the formula; get first 2 and last 2 digits of year

--- src/backend-code.js
+++ src/backend-code.js
@@ -3,10 +3,21 @@ export class Date {
     this.year = year;
     this.month = month;
     this.dayOfMonth = dayOfMonth;
+    this.y;
+    this.c;
   }
 
-  convertMonth() {
+  convertDate() {
+    if (this.month === "January" || this.month === "February") {
+      this.year = this.year -1;
+    }
+    this.c = this.year.toString();
+    this.c = parseInt(this.c.charAt(0) + this.c.charAt(1));
+    this.y = this.year.toString();
+    this.y = parseInt(this.y.charAt(2) + this.y.charAt(3));
+
     const months = ["March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February"]
     this.month = months.indexOf(this.month) + 1;
   }
+
 }
