cdd71d5038bcbb395fcf208f3eec612b1c22e652
Jessica Munoz and Na Hyung Choi <na@>
2019-06-27 14:56:51 -0700

    Built out UI logic, index.html file and tested interface

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Constructor-Name } from './backend-code';
+import { monthToNum, makeDate, getDayOfWeek, convertDay } from './backend-code';
 
 $(function() {
+  $("form#make-date").submit(function(event) {
+    event.preventDefault();
 
+    let inputMonth = $("#month").val();
+    let inputDay = $("#day").val();
+    let inputYear = $("#year").val();
+    let monthNum = monthToNum(inputMonth);
+
+    let output = convertDay(getDayOfWeek(makeDate(inputYear, monthNum, inputDay)));
+    $("#stringOutput").text(output);
+    console.log(output);
+  });
 });
